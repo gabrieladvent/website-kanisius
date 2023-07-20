ecd11eb76b02abc2f9ba6fbe55b80ca9c2a44cbf
upii26 <balefighet@gmail.com>
2023-07-20 12:11:24 +0700

    middleware mati

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -22,24 +22,42 @@ class RoleMiddleware
     //     }
     //     return $next($request);
     // }
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next  )
     {
-        // Periksa apakah pengguna telah login
-        if (Auth::check()) {
-            // Periksa peran pengguna
-            if (Auth::user()->status == "yayasan") {
-                // Jika pengguna memiliki peran pertama, arahkan ke halaman A
-                return response()->view('/dashboard');
+    //     // Periksa apakah pengguna telah login
+    //     if (Auth::check()) {
+    //         // Periksa peran pengguna
+    //         if (Auth::user()->status == "yayasan") {
+    //             // Jika pengguna memiliki peran pertama, arahkan ke halaman A
+    //             return response()->view('/dashboard');
                 
-            } elseif (Auth::user()->status == "sekolah") {
-                // Jika pengguna memiliki peran kedua, arahkan ke halaman B
+    //         } elseif (Auth::user()->status == "sekolah") {
+    //             // Jika pengguna memiliki peran kedua, arahkan ke halaman B
+    //             return response()->view('/homesekolah');
+    //         }
+    //     }
+
+    //     // Jika pengguna tidak memiliki peran yang valid atau belum login,
+    //     // Anda dapat mengarahkannya ke halaman tertentu atau memberikan respons sesuai kebutuhan.
+    //     return response()->view('/home');
+    // }
+
+    
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            // Periksa peran pengguna
+            if ($user->status == 'yayasan') {
+                // Jika peran adalah 'admin', arahkan ke halaman dashboard admin
+                return response()->view('/dashboard');
+            } elseif ($user->status == 'sekolah') {
+                // Jika peran adalah 'editor', arahkan ke halaman dashboard editor
                 return response()->view('/homesekolah');
             }
         }
 
-        // Jika pengguna tidak memiliki peran yang valid atau belum login,
-        // Anda dapat mengarahkannya ke halaman tertentu atau memberikan respons sesuai kebutuhan.
-        return response()->view('/login');
-    }
+        return $next($request);
+    
 
+    }
 }
